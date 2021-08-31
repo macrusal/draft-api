110947be439be694fb3a04a82ea33348461fbb34
macrusal <macrusal@gmail.com>
2021-08-31 20:58:18 -0300

    Pontos de injeção @Autowired

--- src/main/java/com/algaworks/draftapi/notificador/NotificacaoEmail.java
+++ src/main/java/com/algaworks/draftapi/notificador/NotificacaoEmail.java
@@ -7,30 +7,13 @@ import org.springframework.stereotype.Component;
  * @author macrusal on 31/08/21
  * @project exemplo-injecao-dependencia
  */
-
+@Component
 public class NotificacaoEmail implements Notificador {
-
-    private boolean caixaAlta;
-    private String hostServidorSMTP;
-
-    public NotificacaoEmail(String hostServidorSMTP) {
-        this.hostServidorSMTP = hostServidorSMTP;
-        System.out.println("NotificacaoEmail");
-    }
 
     @Override
     public void notificar( Cliente cliente, String mensagem ) {
-        if(this.caixaAlta) {
-            mensagem = mensagem.toUpperCase();
-        }
-        System.out.printf("Notificação enviada para o cliente %s através do e-mail %s, servidor SMTP %s : %s \n",
-                cliente.getNome(), cliente.getEmail(), this.hostServidorSMTP, mensagem);
-    }
 
-    /**
-     * Sets caixaAlta
-     */
-    public void setCaixaAlta( boolean caixaAlta ) {
-        this.caixaAlta = caixaAlta;
+        System.out.printf("Notificação enviada para o cliente %s através do e-mail %s: %s \n",
+                cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
--- src/main/java/com/algaworks/draftapi/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/draftapi/service/AtivacaoClienteService.java
@@ -2,23 +2,37 @@ package com.algaworks.draftapi.service;
 
 import com.algaworks.draftapi.modelo.Cliente;
 import com.algaworks.draftapi.notificador.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
  * @author macrusal on 31/08/21
  * @project exemplo-injecao-dependencia
  */
+@Component
 public class AtivacaoClienteService {
 
+    @Autowired
     private Notificador notificador;
 
-    public AtivacaoClienteService( Notificador notificador ) {
-        this.notificador = notificador;
-        System.out.println("AtivacaoClienteService - " + notificador);
-    }
+//    public AtivacaoClienteService() {
+//    }
+//
+//    @Autowired
+//    public AtivacaoClienteService( Notificador notificador ) {
+//        this.notificador = notificador;
+//    }
 
     public void ativar( Cliente cliente ) {
         cliente.ativar();
         this.notificador.notificar(cliente, "Cadastro ativado com sucesso!");
     }
+
+    /**
+     * Sets notificador
+     */
+//    @Autowired
+//    public void setNotificador( Notificador notificador ) {
+//        this.notificador = notificador;
+//    }
 }
