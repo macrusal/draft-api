f040c1802fadf0cee99ebad89c755edfca0adec6
macrusal <macrusal@gmail.com>
2021-09-03 05:38:26 -0300

    Acessando properties do yml via @Value

--- src/main/java/com/algaworks/draftapi/notificador/NotificacaoEmail.java
+++ src/main/java/com/algaworks/draftapi/notificador/NotificacaoEmail.java
@@ -1,6 +1,7 @@
 package com.algaworks.draftapi.notificador;
 
 import com.algaworks.draftapi.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -14,13 +15,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificacaoEmail implements Notificador {
 
+    @Value( "${notificador.email.host-servidor}" )
+    private String host;
+
+    @Value( "${notificador.email.port-servidor}" )
+    private Integer port;
+
     public NotificacaoEmail() {
         System.out.println("Notificador email produção");
     }
 
     @Override
     public void notificar( Cliente cliente, String mensagem ) {
-
+        System.out.println("Servidor.: "+ host);
+        System.out.println("Porta....: "+ port);
         System.out.printf("Notificação enviada para o cliente %s através do e-mail %s: %s \n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
